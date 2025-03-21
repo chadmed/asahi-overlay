chadmed/asahi-overlay

commit 1fc5fd976027e806134bbce4fd3ec19c71caee35

sys-kernel/asahi-kernel: add 6.13.7_p4
Signed-off-by: Janne Grunau <j@jannau.net>

sys-kernel/asahi-kernel/Manifest
@@ -12,4 +12,5 @@ DIST linux-6.12.tar.xz 147906904 BLAKE2B b2ec2fc69218cacabbbe49f78384a5d259ca581
 DIST linux-6.13.tar.xz 148523052 BLAKE2B 9f617ecb3f2393b57ba03c654fea62a7213f24c835989f333a1ef29492af551bfa7d9ad786d5ef1484854adc77c7c6af38fb09a72d994d305695f512c325e77f SHA512 1137e6440132b0958f89165440e99208f82b204e7245ae69dc9c808df97d13ce8f58136db92407e0e93394fa7f6283ec7a34597c6e92a5b6d9025e0960357957
 DIST linux-asahi-6.12.12-1.diff 3458302 BLAKE2B 56186f0291e3b6c81965c1b47aa487bc0d5af4f45e3059e1eafdf94a03aa1e2cf58573b3b62331dc049ed4202a9e2fc213750b4e07670b60c0ac9dcd2a44e52b SHA512 4cf27b52236d1aa6250ffb3b7b0e4a7e485af9f9365ad919d8d21a92fae11fe2bf7893065b137415e840462f8209ea1aecda880dec0b77e61f009902c1725392
 DIST linux-asahi-6.13.5-4.diff 3336046 BLAKE2B c439642460d93d3c65280f39bbc34530203ebcfc8aaa9a542277039bfd2327aca837723eb269061d045318f141634ffbc9bedbccd18d26bf8e9e57b66124db19 SHA512 f45c99d1b139c489122b47e0ec91a28a0969645b293c6b36f26ee976f6a87d36d3aecfdb0f2ab03da331ab5005aa28f5930732352d5ca54c120bc903bf860661
+DIST linux-asahi-6.13.7-3..asahi-6.13.7-4.diff 2565 BLAKE2B 4702eabc477204437faba918dc8284f3d70d17b66424a0b1bb45f93e9ec09ea93f01ad8e1bc15e694bd218bb4869f3c4d6c467dd22f918d8aabc5e89577dbb7e SHA512 d605d841df1328e8a387116be61a3fd99d310678361a65294329264cbdefce6f7a51704a98d8519571c8464b4e41f02ac1401d88463ffbd5f130b5284db2b91f
 DIST linux-asahi-6.13.7-3.diff 3333201 BLAKE2B 2186fed8150603e5f5d564bfd1eb3893764bdf42c8258096ad2c663eb2a8f7330e12714a4c8800dd3864a1150f3178491154791bd5d5a8906ff5666cd3d172cb SHA512 db4edc4f5304cd63327028a2ebc85de8ad00cf5b161710fd373156308d6d1b43b72d88354c593f637f6833a4af6435f37e96dfe2e4da8c7123bebbd1f1c7bfba

sys-kernel/asahi-kernel/asahi-kernel-6.13.7_p4.ebuild
@@ -1,0 +1,152 @@
+# Copyright 2023-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI="8"
+K_SECURITY_UNSUPPORTED="1"
+ETYPE="sources"
+K_NODRYRUN="1"
+
+RUST_MIN_VER="1.78.0"
+RUST_REQ_USE='rust-src,rustfmt'
+
+inherit kernel-build rust
+
+MY_P=linux-${PV%.*}
+GENPATCHES_P="genpatches-$(ver_cut 1-2)-9"
+
+if [[ ${PV} != ${PV/_rc} ]] ; then
+	# $PV is expected to be of following form: 6.0_rc5_p1
+	MY_TAG="$(ver_cut 6)"
+	MY_BASE="$(ver_rs 2 - $(ver_cut 1-4))"
+else
+	# $PV is expected to be of following form: 5.19.0_p1
+	MY_TAG="$(ver_cut 5)"
+	if [[ "$(ver_cut 3)" == "0" ]] ; then
+		MY_BASE="$(ver_cut 1-2)"
+	else
+		MY_BASE="$(ver_cut 1-3)"
+	fi
+fi
+
+# BASE_ASAHI_TAG is the first used TAG of specific release, i.e. usually
+# the first tag of a linux 6.x or linux stable 6.x.y release
+BASE_ASAHI_TAG="asahi-${MY_BASE}-3"
+ASAHI_TAG="asahi-${MY_BASE}-${MY_TAG}"
+
+CONFIG_VER=6.13.7-401
+GENTOO_CONFIG_VER=g15
+# provide a temporary mirror as long as Fedora's copr dit-git cgit is dissabled
+FEDORA_CONFIG_DISTGIT="asahi.jannau.net/cgit/@asahi/kernel"
+# FEDORA_CONFIG_DISTGIT="copr-dist-git.fedorainfracloud.org/cgit/@asahi/kernel"
+# FEDORA_CONFIG_DISTGIT="copr-dist-git.fedorainfracloud.org/cgit/ngompa/fedora-asahi-dev"
+FEDORA_CONFIG_SHA1=01bc2d858c0bbeb190205e733b777aa0fb1a2e0d
+
+DESCRIPTION="Asahi Linux kernel sources"
+SLOT="asahi/${PVR}"
+HOMEPAGE="https://asahilinux.org"
+SRC_URI="
+    https://cdn.kernel.org/pub/linux/kernel/v$(ver_cut 1).x/${MY_P}.tar.xz
+    https://github.com/AsahiLinux/linux/compare/v${MY_BASE}...${BASE_ASAHI_TAG}.patch
+        -> linux-${BASE_ASAHI_TAG}.diff
+    https://github.com/AsahiLinux/linux/compare/${BASE_ASAHI_TAG}...${ASAHI_TAG}.patch
+        -> linux-${BASE_ASAHI_TAG}..${ASAHI_TAG}.diff
+    https://dev.gentoo.org/~mpagano/dist/genpatches/${GENPATCHES_P}.base.tar.xz
+    https://dev.gentoo.org/~mpagano/dist/genpatches/${GENPATCHES_P}.extras.tar.xz
+    https://github.com/projg2/gentoo-kernel-config/archive/${GENTOO_CONFIG_VER}.tar.gz
+        -> gentoo-kernel-config-${GENTOO_CONFIG_VER}.tar.gz
+	https://${FEDORA_CONFIG_DISTGIT}/kernel.git/plain/kernel-aarch64-16k-fedora.config?id=${FEDORA_CONFIG_SHA1}
+		-> kernel-aarch64-16k-fedora.config-${CONFIG_VER}
+"
+
+S="${WORKDIR}/${MY_P}"
+
+LICENSE="GPL-2"
+KEYWORDS="~arm64"
+
+IUSE="debug"
+
+# Rust is non-negotiable for the dist kernel
+DEPEND="
+	${DEPEND}
+	dev-util/bindgen
+	debug? ( dev-util/pahole )
+"
+
+PDEPEND="
+	~virtual/dist-kernel-${PV}
+"
+
+QA_FLAGS_IGNORED="
+	usr/src/linux-.*/scripts/gcc-plugins/.*.so
+	usr/src/linux-.*/vmlinux
+	usr/src/linux-.*/arch/powerpc/kernel/vdso.*/vdso.*.so.dbg
+"
+
+src_prepare() {
+    local PATCHES=(
+        # meh, genpatches have no directory
+        "${WORKDIR}"/*.patch
+        "${FILESDIR}/2980_revert_kbuild-gcc15-gnu23-to-gnu11-fix.patch"
+        "${FILESDIR}/1740_revert_x86-insn-decoder-test-allow-longer-symbol-names.patch"
+        "${DISTDIR}/linux-${BASE_ASAHI_TAG}.diff"
+        "${DISTDIR}/linux-${BASE_ASAHI_TAG}..${ASAHI_TAG}.diff"
+        "${FILESDIR}/2981-kbuild-gcc15-gnu23-to-gnu11-fix.patch"
+        "${FILESDIR}/${PN}-6.8-config-gentoo-Drop-RANDSTRUCT-from-GENTOO_KERNEL_SEL.patch"
+    )
+	default
+
+	# prepare the default config
+	cp "${DISTDIR}/kernel-aarch64-16k-fedora.config-${CONFIG_VER}" ".config" || die
+
+	# ensure a consistant version across kernel and gentoo
+	# this passes the ${PV}-as-release check in kernel-install_pkg_preinst()
+	# override "-asahi" in localversion.05-asahi with "_pX" to override the
+	# kernel's base varsion to gentoo's ${PV}
+	echo "-p${MY_TAG}" > localversion.05-asahi
+	# use CONFIG_LOCALVERSION to provide "asahi" and "dist" annotations.
+	local myversion="-asahi-dist"
+	echo "CONFIG_LOCALVERSION=\"${myversion}\"" > "${T}"/version.config || die
+	local dist_conf_path="${WORKDIR}/gentoo-kernel-config-${GENTOO_CONFIG_VER}"
+
+	local merge_configs=(
+		"${T}"/version.config
+		"${dist_conf_path}"/base.config
+	)
+	use debug || merge_configs+=(
+		"${dist_conf_path}"/no-debug.config
+		"${FILESDIR}"/linux-6.10_disable_debug_info_btf.config
+	)
+
+	# deselect all non APPLE arm64 ARCHs
+	merge_configs+=(
+		"${FILESDIR}"/linux-6.8_arm64_deselect_non_apple_arch.config
+	)
+	# adjust base config for Apple silicon systems
+	merge_configs+=(
+		"${FILESDIR}"/linux-6.8_arch_apple_overrides.config
+	)
+
+	# amdgpu no longer builds with clang (issue #113)
+	merge_configs+=(
+		"${FILESDIR}"/linux-6.10_drop_amdgpu.config
+	)
+
+	kernel-build_merge_configs "${merge_configs[@]}"
+}
+
+src_install() {
+	# call kernel-build's scr_install
+	kernel-build_src_install
+
+	# symlink installed *.dtbs back into kernel "source" directory
+	for dtb in "${ED}/lib/modules/${KV_FULL}/dtb/apple/"*.dtb; do
+		local basedtb=$(basename ${dtb})
+		dosym -r "${dtb##${ED}}" "/usr/src/linux-${KV_FULL}/arch/arm64/boot/dts/apple/${basedtb}"
+	done
+}
+
+pkg_postinst() {
+	einfo "For more information about Asahi Linux please visit ${HOMEPAGE},"
+	einfo "or consult the Wiki at https://github.com/AsahiLinux/docs/wiki."
+	kernel-build_pkg_postinst
+}

commit 8d18037685fbc1625111bf4214822a3a288506ee

sys-kernel/asahi-kernel: update SRC_URI
Fixes: 1fc5fd976027 ("sys-kernel/asahi-kernel: add 6.13.7_p4")
Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

sys-kernel/asahi-kernel/Manifest
@@ -5,12 +5,12 @@ DIST genpatches-6.13-7.extras.tar.xz 4056 BLAKE2B 955e08c33032fbb70ab68d49a52fa3
 DIST genpatches-6.13-9.base.tar.xz 591648 BLAKE2B aca872a5d7956455b59fd50ea22b3b5e1da834506d9388614a36103fb2afdb1c7a02d75a8317378a9b6f934f3e5c294d5ffcadfb92d4d4850c7e865f3df18d0b SHA512 9fa7338ee164cd22c8cd0c5f9b133d64db60e8029232cb12ff8219a5a124ad0563673347639507b3ea6be66b5c42918bade3ff5b0515e44577120b8e2da55249
 DIST genpatches-6.13-9.extras.tar.xz 4060 BLAKE2B f4ce8b701cafe532570d8066e2bc0a41eb2340f54a078a8766cd5f1e0c4e2ae5cbd17f6db1cea0b045ec92dc492d7a9886e9e0513adbc9bf5f3012efb9d7eee0 SHA512 43a23e9d3ad03b31a9de22270b89d6abf78247bbd9ad7a60e8d381c09affdb26e11041aa6cacaeef9d44be5b1b976d6d201047b3a6c52dd3e389633d525d013e
 DIST gentoo-kernel-config-g15.tar.gz 5746 BLAKE2B 2baef40e18cbf3bb975362d71ad674604ad80338d1e6914ffa2fc03af26f1b9c9cec66d39a56077693a7327060bc29bd5eb5787549857dd3779e841192cb2016 SHA512 45d609ee25a529988868b8c99c808f4e6abbde34ae501bf35523431ee85c9f3657663d6f938f2036a5f012a55a68965b32fd41fc44d8f6ca606a6fd38f952445
-DIST kernel-aarch64-16k-fedora.config-6.12.12-400-gentoo 289140 BLAKE2B 66bfc82c99622a61c53ce8ad4a85cce2ff8618055bd43e4540fdfdbcd57e9ea515e47d195b905491b267bb95c76100874067af02a4e7b2e80a2bbfb7956066bd SHA512 fdc4be28e88936e41cedbfe4e0ebbe60d3c1d0b7180bfb955218eee11f245b5730e0bb125e61db9c002bf962697a9bba19df1047bc40cf4cec21b491b9eabd95
+DIST kernel-aarch64-16k-fedora.config-6.12.12-400-gentoo 290544 BLAKE2B 9507f5c20f03de1a485be0841cc7b4c0ae5b37530d40ded296f057deb49aab3c9a5839e2a60450ce8e1a32b85262bce66daa7a42b6017336101fccc15726fe9f SHA512 1102c3ec6ab2fc790feb7def9871e2e12b0f492654c0d58ec4ab10410e3b31950425ad86ec160aca76d158a50604e8eb1bdb2fcc1680b64572774d6ac707e07e
 DIST kernel-aarch64-16k-fedora.config-6.13.5-400 290544 BLAKE2B 9507f5c20f03de1a485be0841cc7b4c0ae5b37530d40ded296f057deb49aab3c9a5839e2a60450ce8e1a32b85262bce66daa7a42b6017336101fccc15726fe9f SHA512 1102c3ec6ab2fc790feb7def9871e2e12b0f492654c0d58ec4ab10410e3b31950425ad86ec160aca76d158a50604e8eb1bdb2fcc1680b64572774d6ac707e07e
 DIST kernel-aarch64-16k-fedora.config-6.13.7-401 290544 BLAKE2B 9507f5c20f03de1a485be0841cc7b4c0ae5b37530d40ded296f057deb49aab3c9a5839e2a60450ce8e1a32b85262bce66daa7a42b6017336101fccc15726fe9f SHA512 1102c3ec6ab2fc790feb7def9871e2e12b0f492654c0d58ec4ab10410e3b31950425ad86ec160aca76d158a50604e8eb1bdb2fcc1680b64572774d6ac707e07e
 DIST linux-6.12.tar.xz 147906904 BLAKE2B b2ec2fc69218cacabbbe49f78384a5d259ca581b717617c12b000b16f4a4c59ee348ea886b37147f5f70fb9a7a01c1e2c8f19021078f6b23f5bc62d1c48d5e5e SHA512 a37b1823df7b4f72542f689b65882634740ba0401a42fdcf6601d9efd2e132e5a7650e70450ba76f6cd1f13ca31180f2ccee9d54fe4df89bc0000ade4380a548
 DIST linux-6.13.tar.xz 148523052 BLAKE2B 9f617ecb3f2393b57ba03c654fea62a7213f24c835989f333a1ef29492af551bfa7d9ad786d5ef1484854adc77c7c6af38fb09a72d994d305695f512c325e77f SHA512 1137e6440132b0958f89165440e99208f82b204e7245ae69dc9c808df97d13ce8f58136db92407e0e93394fa7f6283ec7a34597c6e92a5b6d9025e0960357957
 DIST linux-asahi-6.12.12-1.diff 3458302 BLAKE2B 56186f0291e3b6c81965c1b47aa487bc0d5af4f45e3059e1eafdf94a03aa1e2cf58573b3b62331dc049ed4202a9e2fc213750b4e07670b60c0ac9dcd2a44e52b SHA512 4cf27b52236d1aa6250ffb3b7b0e4a7e485af9f9365ad919d8d21a92fae11fe2bf7893065b137415e840462f8209ea1aecda880dec0b77e61f009902c1725392
 DIST linux-asahi-6.13.5-4.diff 3336046 BLAKE2B c439642460d93d3c65280f39bbc34530203ebcfc8aaa9a542277039bfd2327aca837723eb269061d045318f141634ffbc9bedbccd18d26bf8e9e57b66124db19 SHA512 f45c99d1b139c489122b47e0ec91a28a0969645b293c6b36f26ee976f6a87d36d3aecfdb0f2ab03da331ab5005aa28f5930732352d5ca54c120bc903bf860661
-DIST linux-asahi-6.13.7-3..asahi-6.13.7-4.diff 2565 BLAKE2B 4702eabc477204437faba918dc8284f3d70d17b66424a0b1bb45f93e9ec09ea93f01ad8e1bc15e694bd218bb4869f3c4d6c467dd22f918d8aabc5e89577dbb7e SHA512 d605d841df1328e8a387116be61a3fd99d310678361a65294329264cbdefce6f7a51704a98d8519571c8464b4e41f02ac1401d88463ffbd5f130b5284db2b91f
+DIST linux-asahi-6.13.7-3..asahi-6.13.7-4.diff 1992 BLAKE2B 2c7dff586b3e2d3eb9d06189ecb28a233d093451240e6cc03f43f8b42ce5351d19d9b51aafecfe5a64152f9c9f527abc1c0656c0799351a57b488650044bf7ad SHA512 a429810c2095ccc82838364224409070e97917ddd4e128554f6db3f32edbc4240240d2e58e1b75d75909301cc962f1be9d8db3bd535a06b03d2103c789d47296
 DIST linux-asahi-6.13.7-3.diff 3333201 BLAKE2B 2186fed8150603e5f5d564bfd1eb3893764bdf42c8258096ad2c663eb2a8f7330e12714a4c8800dd3864a1150f3178491154791bd5d5a8906ff5666cd3d172cb SHA512 db4edc4f5304cd63327028a2ebc85de8ad00cf5b161710fd373156308d6d1b43b72d88354c593f637f6833a4af6435f37e96dfe2e4da8c7123bebbd1f1c7bfba

sys-kernel/asahi-kernel/asahi-kernel-6.13.7_p4.ebuild
@@ -46,9 +46,9 @@ SLOT="asahi/${PVR}"
 HOMEPAGE="https://asahilinux.org"
 SRC_URI="
     https://cdn.kernel.org/pub/linux/kernel/v$(ver_cut 1).x/${MY_P}.tar.xz
-    https://github.com/AsahiLinux/linux/compare/v${MY_BASE}...${BASE_ASAHI_TAG}.patch
+    https://github.com/AsahiLinux/linux/compare/v${MY_BASE}...${BASE_ASAHI_TAG}.diff
         -> linux-${BASE_ASAHI_TAG}.diff
-    https://github.com/AsahiLinux/linux/compare/${BASE_ASAHI_TAG}...${ASAHI_TAG}.patch
+    https://github.com/AsahiLinux/linux/compare/${BASE_ASAHI_TAG}...${ASAHI_TAG}.diff
         -> linux-${BASE_ASAHI_TAG}..${ASAHI_TAG}.diff
     https://dev.gentoo.org/~mpagano/dist/genpatches/${GENPATCHES_P}.base.tar.xz
     https://dev.gentoo.org/~mpagano/dist/genpatches/${GENPATCHES_P}.extras.tar.xz